fdbbfd4533f1bc2c9d3bd7a1ba7210116c4a1d98
noe1234567890 <noekakhetelidze498@gmail.com>
2024-07-27 18:03:53 +0400

    third project

--- DAY11/homework2.py
+++ DAY11/homework2.py
@@ -1,20 +1,20 @@
-print(20==20)
-print(20==15)
-print(20>10)
-print(30>50)
-print(500<20)
-print(500<501)
-age1=int(input("please enter your age for the youtube kids!: "))
+print(20==20)#თუ ტოლია true არის
+print(20==15)#თუ ტოლი არ არის false არის
+print(20>10)#თუ მეტია true არის
+print(30>50)#თუ მეტი არ არის false არის
+print(500<20)#თუ ნაკლებია არ არის false არის
+print(500<501)#თუ ნაკლებია true არის
+age1=int(input("please enter your age for the youtube kids!: "))#ინფუთებთანაც შეიძლება
 enterage=7
-print(age1<=enterage)
+print(age1<=enterage)#თუ ნაკლებია ან ტოლია true არის
 age2=int(input("please input your age: "))
-print(age2>=18)
+print(age2>=18)#თუ მეტია ან ტოლია true არის
 money=int(input("please enter you balance: "))
 loptop=500
-print(money>=loptop)
-print(True and True)
-print(False and False)
-print(True and False)
-print(True or True)
-print(False or False)
-print(False or True)
+print(money>=loptop)#ეს მსგავსი მაგალითი ოღონდ მეორე ცვლადით
+print(True and True)#თუ ორივე true არის მაშინ გამოდის true
+print(False and False)#იგივე ოღონდ ორ false-ს გამოაქვს false
+print(True and False)#ანდ false-ისკენ იხრება და გამოდის false
+print(True or True)#აქ true იმიტომ რომ ორივე true-არის
+print(False or False)#თუ ორივე false არის გამოდის false
+print(False or True)#ორ true-სკენ იხრება და გამოდის true
